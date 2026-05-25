79935b6181dce66327315886e63069d93301d80b
Ashutosh Dash <codingashutosh441@gmail.com>
2026-05-25 10:51:53 +0530

    Implemented basic SQL parser flow with grammar validation and SELECT * semantic handling

--- main.cpp
+++ main.cpp
@@ -2,9 +2,19 @@
 #include <algorithm>
 #include <sstream>
 #include <set>
-#include<cctype>
+#include <vector>
+#include <cctype>
+
 using namespace std;
 
+struct Token {
+    string value;
+    string type;
+};
+
+vector<Token> tokens;
+int pos = 0;
+bool selectAllColumns = false;
 
 bool isNumber(string s) {
 
@@ -19,6 +29,7 @@ bool isNumber(string s) {
 
     return true;
 }
+
 bool isIdentifier(string s) {
 
     if(s.empty())
@@ -38,65 +49,182 @@ bool isIdentifier(string s) {
 
 set<string> mysqlKeywords = {
 
-    "ADD", "ALL", "ALTER", "ANALYZE", "AND",
-    "AS", "ASC", "AUTO_INCREMENT",
-    "BETWEEN", "BY",
-    "CASE", "CHECK", "COLUMN", "CONSTRAINT",
-    "CREATE", "CROSS",
-    "DATABASE", "DEFAULT", "DELETE", "DESC",
-    "DISTINCT", "DROP",
-    "ELSE", "END", "EXISTS",
-    "FOREIGN", "FROM", "FULL",
-    "GROUP",
-    "HAVING",
-    "IN", "INDEX", "INNER", "INSERT", "INTO",
-    "IS",
-    "JOIN",
-    "KEY",
-    "LEFT", "LIKE", "LIMIT",
-    "NOT", "NULL",
-    "ON", "OR", "ORDER", "OUTER",
-    "PRIMARY", "PROCEDURE",
-    "REFERENCES", "RIGHT", "ROLLBACK",
-    "SELECT", "SET",
-    "TABLE", "THEN", "TO", "TRANSACTION",
-    "TRUNCATE",
-    "UNION", "UNIQUE", "UPDATE", "USE",
-    "VALUES", "VIEW",
-    "WHEN", "WHERE"
+    "SELECT", "FROM", "WHERE",
+    "INSERT", "UPDATE", "DELETE",
+    "ORDER", "BY", "INTO",
+    "VALUES", "SET"
+};
 
-};
 set<string> ops = {
-        "+", "-", "*", "/",
-        "=", "==", "!=", "<", ">",
-        "<=", ">=", "&&", "||", "%"
+
+    "+", "-", "*", "/",
+    "=", "==", "!=", "<", ">",
+    "<=", ">=", "%"
 };
+
+bool expect(string val) {
+
+    if(pos < tokens.size() &&
+       tokens[pos].value == val) {
+
+        pos++;
+        return true;
+    }
+
+    return false;
+}
+
+bool parseColumns() {
+
+    if(pos >= tokens.size())
+        return false;
+
+    if(tokens[pos].value == "*"){
+        selectAllColumns = true;
+        pos++;
+        return true;
+    }
+
+    if(tokens[pos].type != "identifier")
+        return false;
+
+    pos++;
+
+    while(pos < tokens.size() &&
+          tokens[pos].value == ",") {
+
+        pos++;
+
+        if(pos >= tokens.size())
+            return false;
+
+        if(tokens[pos].type != "identifier")
+            return false;
+
+        pos++;
+    }
+
+    return true;
+}
+
+bool parseWhere() {
+
+    if(pos < tokens.size() &&
+       tokens[pos].value == "WHERE") {
+
+        pos++;
+
+        if(pos >= tokens.size() ||
+           tokens[pos].type != "identifier")
+            return false;
+
+        pos++;
+
+        if(pos >= tokens.size() ||
+           tokens[pos].type != "operator")
+            return false;
+
+        pos++;
+
+        if(pos >= tokens.size())
+            return false;
+
+        if(tokens[pos].type != "digit" &&
+           tokens[pos].type != "string" &&
+           tokens[pos].type != "identifier")
+            return false;
+
+        pos++;
+    }
+
+    return true;
+}
+
+bool parseSelect() {
+
+    if(!expect("SELECT")) {
+        cout << "Syntax Error : SELECT expected\n";
+        return false;
+    }
+
+    if(!parseColumns()) {
+        cout << "Syntax Error : Invalid column list\n";
+        return false;
+    }
+
+    if(!expect("FROM")) {
+        cout << "Syntax Error : FROM expected\n";
+        return false;
+    }
+
+    if(pos >= tokens.size() ||
+       tokens[pos].type != "identifier") {
+
+        cout << "Syntax Error : Table name expected\n";
+        return false;
+    }
+
+    pos++;
+
+    if(!parseWhere()) {
+        cout << "Syntax Error : Invalid WHERE clause\n";
+        return false;
+    }
+
+    if(pos < tokens.size() &&
+       tokens[pos].value == ";") {
+
+        pos++;
+    }
+
+    if(pos != tokens.size()) {
+        cout << "Syntax Error : Extra tokens found\n";
+        return false;
+    }
+
+    return true;
+}
+
 int main() {
 
     string s;
 
     getline(cin, s);
-    
-
     string updated = "";
 
     for(int i = 0; i < s.length(); i++) {
 
-       if(i + 1 < s.length()) {
+        if(s[i] == '\'') {
+
+            string str = "'";
+            i++;
+
+            while(i < s.length() &&
+                  s[i] != '\'') {
+
+                str += s[i];
+                i++;
+            }
+
+            str += "'";
+            updated += " " + str + " ";
+            continue;
+        }
+
+        if(i + 1 < s.length()) {
 
             string two = s.substr(i, 2);
 
             if(two == ">=" || two == "<=" ||
-            two == "==" || two == "!=" ||
-            two == "<>") {
+               two == "==" || two == "!=") {
 
-            updated += ' ';
-            updated += two;
-            updated += ' ';
+                updated += " ";
+                updated += two;
+                updated += " ";
 
-            i++; // skip next character
-            continue;
-           }
+                i++;
+                continue;
+            }
         }
 
         if(s[i] == ',' || s[i] == ';' ||
@@ -106,18 +234,20 @@ int main() {
            s[i] == '=' || s[i] == '<' ||
            s[i] == '>') {
 
-           updated += ' ';
-           updated += s[i];
-           updated += ' ';
+            updated += " ";
+            updated += s[i];
+            updated += " ";
         }
+
         else {
-           updated += s[i];
+            updated += s[i];
         }
     }
 
     stringstream ss(updated);
 
     string word;
+
     while(ss >> word) {
 
         string temp = word;
@@ -126,31 +256,77 @@ int main() {
                   temp.begin(), ::toupper);
 
         if(mysqlKeywords.count(temp)) {
-            cout << word << " -> Keyword" << endl;
+
+            tokens.push_back({temp, "keyword"});
         }
 
         else if(isIdentifier(word)) {
-            cout << word << " -> Identifier" << endl;
+
+            tokens.push_back({word, "identifier"});
         }
 
-        else if(isNumber(word)){
-             cout << word << " -> Digit" << endl;
+        else if(isNumber(word)) {
+
+            tokens.push_back({word, "digit"});
+        }
+
+        else if(word.front() == '\'' &&
+                word.back() == '\'') {
+
+            tokens.push_back({word, "string"});
         }
 
         else if(word == ",") {
-            cout << word << " -> Comma" << endl;
+
+            tokens.push_back({word, "comma"});
         }
 
         else if(word == ";") {
-            cout << word << " -> Semicolon" << endl;
+
+            tokens.push_back({word, "semicolon"});
         }
 
-        else if(ops.count(word)){
-            cout << word << " -> operator" << endl;
+        else if(word == "(") {
+
+            tokens.push_back({word, "lparen"});
         }
-        else{
-            cout << word << " -> Unknown" << endl;
+
+        else if(word == ")") {
+
+            tokens.push_back({word, "rparen"});
         }
+
+        else if(ops.count(word)) {
+
+            tokens.push_back({word, "operator"});
+        }
+
+        else {
+
+            tokens.push_back({word, "unknown"});
+        }
+    }
+
+    cout << "\nTOKENS\n\n";
+
+    for(const auto& t : tokens) {
+
+        cout << t.value
+             << " -> "
+             << t.type
+             << endl;
+    }
+
+    cout << "\nPARSING RESULT\n\n";
+
+    if(parseSelect()) {
+
+        cout << "Valid SELECT Query\n";
+    }
+
+    else {
+
+        cout << "Invalid Query\n";
     }
 
     return 0;
